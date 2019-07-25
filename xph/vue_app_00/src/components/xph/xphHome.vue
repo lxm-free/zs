4c7d3e955fc35f0386c1771fb21bd8b8011f1885
lxm-free <52703328+lxm-free@users.noreply.github.com>
2019-07-25 16:29:15 +0800

    Update xphHome.vue

--- xph/vue_app_00/src/components/xph/xphHome.vue
+++ xph/vue_app_00/src/components/xph/xphHome.vue
@@ -15,22 +15,28 @@
         <!-- 底部导航栏 -->
         <mt-tabbar v-model="selected" fixed> 
             <mt-tab-item id="home" @click.native="changeState(0)" >
-                <tabbaricon  :selectedImage="require('../../assets/1/sy2.png')" :normalImage="require('../../assets/1/sy1.png')" :focused="currentIndex[0].isSelect"></tabbaricon> 
+                <tabbaricon  :selectedImage="require('../../assets/1/sy2.png')" :normalImage="require('../../assets/1/sy1.png')"
+                :focused="currentIndex[0].isSelect"></tabbaricon> 
                 首页
             </mt-tab-item>
             <mt-tab-item id="group_by" @click.native="changeState(1)">
-                <tabbaricon  :selectedImage="require('../../assets/1/tg2.png')" :normalImage="require('../../assets/1/tg1.png')" :focused="currentIndex[1].isSelect"></tabbaricon>
+                <tabbaricon  :selectedImage="require('../../assets/1/tg2.png')" :normalImage="require('../../assets/1/tg1.png')"
+                :focused="currentIndex[1].isSelect"></tabbaricon>
                 团购
             </mt-tab-item>
             <mt-tab-item id="me" @click.native="changeState(2)">
-                <tabbaricon  :selectedImage="require('../../assets/1/wd2.png')" :normalImage="require('../../assets/1/wd1.png')" :focused="currentIndex[2].isSelect"></tabbaricon>
+                <tabbaricon  :selectedImage="require('../../assets/1/wd2.png')" :normalImage="require('../../assets/1/wd1.png')" 
+                :focused="currentIndex[2].isSelect"></tabbaricon>
                 我的
             </mt-tab-item>
     </mt-tabbar>
     </div>
 </template>
 <script>
-const setHtmlFontSize = () => { const htmlDom = document.getElementsByTagName('html')[0]; let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth; if (htmlWidth >= 750) { htmlWidth = 750; } if (htmlWidth <= 320) { htmlWidth = 320; } htmlDom.style.fontSize = `${htmlWidth / 7.5}px`; }; window.onresize = setHtmlFontSize; setHtmlFontSize();
+const setHtmlFontSize = () => { const htmlDom = document.getElementsByTagName('html')[0]; 
+let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth; if (htmlWidth >= 750) { htmlWidth = 750; } 
+if (htmlWidth <= 320) { htmlWidth = 320; } htmlDom.style.fontSize = `${htmlWidth / 7.5}px`; }; window.onresize = setHtmlFontSize;
+setHtmlFontSize();
 import tabBarIcon from "./common/tabBarIcon"
 import Home from "./Home"
 import tgHome from "./tgHome"
